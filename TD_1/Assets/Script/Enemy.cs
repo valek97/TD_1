32c8d1761bdcbdff7bbf0a7190eeaabadc24d770
valek97 <kritenko97@gmail.com>
2019-10-31 00:07:22 +0300

    2 урок. Создание мобов, настройка пути, спавн.

---
+++ TD_1/Assets/Script/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+    public float speed = 2f;
+
+    private Transform waypoints;
+    private Transform waypoint;
+    private int waypointIndex = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = GameObject.Find("WayPoint").transform;
+        NextWaypoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = waypoint.transform.position - transform.position;
+        dir.y = 0;
+
+        float _speed = Time.deltaTime * speed;
+        transform.Translate(dir.normalized * Time.deltaTime * speed);
+        if (dir.magnitude <= _speed)
+        {
+            NextWaypoint();
+        }
+
+    }
+
+    void NextWaypoint()
+    {
+        waypointIndex++;
+        if (waypointIndex >= waypoints.childCount)
+        {
+            waypointIndex = 0;
+            Destroy(gameObject);
+            return;
+        }
+        waypoint = waypoints.GetChild(waypointIndex);
+    }
+}
--- TD_1/Assets/Script/Spawner.cs
+++ TD_1/Assets/Script/Spawner.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject spawnObject;
+    public float spawnTime = 1f;
+
+    private float timer = 0;
     void Start()
     {
         
@@ -13,6 +18,15 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
+
+        if (timer <=0)
+        {
+            Instantiate(spawnObject, transform.position, transform.rotation);
+
+
+            timer = spawnTime;
+        }
         
     }
 }
