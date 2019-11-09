7a185a49fdb8d2d01efd177e3f506b01aea8dc4b
valek97 <kritenko97@gmail.com>
2019-11-09 16:53:20 +0300

    Создание башен, исправил точки пути. Изменение цвета площадок под башнями.

---
+++ TD_1/Assets/Script/CameraScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    Camera camera;
+    TowerPlace currTP;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "TowerPlace")
+        {
+            bool isMouseDown = Input.GetMouseButtonDown(0);
+            bool isMouseUp = Input.GetMouseButtonUp(0);
+
+            TowerPlace tp = hit.collider.gameObject.GetComponent<TowerPlace>();
+            if (isMouseDown || isMouseUp)
+            {
+            
+                    if (isMouseDown)
+                    {
+                        tp.MouseDown();
+                    }
+                    else if (isMouseUp)
+                    {
+                        tp.MouseUp();
+                    }
+
+
+                
+            }
+            else 
+            {
+                if (currTP != tp)
+                {
+                    if (currTP != null)
+                        currTP.MouseLeave();
+                }
+
+                tp.MouseEnter();
+                currTP = tp;
+            }
+        }
+        else
+        {
+            if (currTP != null)
+                currTP.MouseLeave();
+
+            currTP = null;
+        }
+    }
+}
---
+++ TD_1/Assets/Script/TowerPlace.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlace : MonoBehaviour
+{
+    //Можно строить?
+    bool isCanBuild = true;
+    public GameObject tower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Клавиша мыши нажата, курсор находится над объектом
+    public void MouseDown()
+    {
+        if (isCanBuild)
+        {
+            isCanBuild = false;
+            Instantiate(tower, transform.position, transform.rotation);
+            GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+        }
+    }
+
+
+
+    //Клавиша мыши отпущена, курсор находится над объектом
+    public void MouseUp()
+    {
+       
+    }
+
+
+    //Если курсор мыши оказался над объектом
+    public void MouseEnter()
+    {
+        if (isCanBuild)
+            GetComponent<Renderer>().material.color = new Color(1, 0, 0);
+    }
+
+
+
+    //Если курсор мыши не находится над объектом
+    public void MouseLeave()
+    {
+        if (isCanBuild)
+            GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+    }
+}
